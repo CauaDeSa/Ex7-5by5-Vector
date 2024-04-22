efd975e7adb8f0523d24126416e651befb9bc5c9
Cauã Rufino de Sá <cauarufinodesa@gmail.com>
2024-04-22 14:28:08 -0300

    Add project files.

---
+++ Ex7-5by5-Vector/Program.cs
@@ -0,0 +1,35 @@
+﻿int size = 10;
+int[] numbers = new int[size];
+
+Console.WriteLine("Value insert\n");
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"Type the {i + 1}° value: ");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("\nEven values");
+
+for (int i = 0; i < size; i += 2)
+{
+    Console.Write($"{numbers[i]} ");
+}
+
+Console.WriteLine("\nOdd values");
+
+for (int i = 1; i < size; i += 2)
+{
+    Console.Write($"{numbers[i]} ");
+}
+
+Console.WriteLine("\nFirst and last positions");
+
+for (int i = 0; i < size; i++)
+{
+    if (i == 0 || i == size - 1)
+        Console.Write($"{numbers[i]} ");
+}
+
+Console.WriteLine("\n\nPress any key to exit...");
+Console.ReadKey();
